bfab5c948443f977557b09ab8424a9a7615f4a9b
axelmz <axel.melendez@alumnos.udg.mx>
2021-11-08 11:27:00 -0600

    Step 3: Catch invalid hashes

---
+++ webapp/controller/NotFound.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+ ], function (Controller) {
+    "use strict";
+    return Controller.extend("navrout.controller.NotFound", {
+       onInit: function () {
+       }
+    });
+ });
